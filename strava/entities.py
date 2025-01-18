93c923111eda3a9d580f5ec3218b769de32bcdc4
Santosh Gummidipundi <santosh@santoshg.io>
2025-01-17 20:22:28 -0800

    fix strava token enum url

--- strava/entities.py
+++ strava/entities.py
@@ -5,6 +5,6 @@ from enum import Enum
 class StravaURLs(Enum):
     """Strava specific URLs."""
 
-    AUTH_URL = "https://www.strava.com/oauth/authorize"
+    AUTH_URL = "https://www.strava.com/api/v3/oauth/token"
     ATHLETE_URL = "https://www.strava.com/api/v3/athlete"
     ACTIVITY_URL = "https://www.strava.com/api/v3/activity"
