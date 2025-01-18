ea53a39faa2fd4bd92d28fe3441155a46c725a4a
Santosh Gummidipundi <santosh@santoshg.io>
2025-01-17 21:11:44 -0800

    add strava literal as an enum

--- strava/entities.py
+++ strava/entities.py
@@ -8,3 +8,9 @@ class StravaURLs(Enum):
     AUTH_URL = "https://www.strava.com/api/v3/oauth/token"
     ATHLETE_URL = "https://www.strava.com/api/v3/athlete"
     ACTIVITY_URL = "https://www.strava.com/api/v3/activity"
+
+
+class StravaKeys(Enum):
+    """Strava specific keys."""
+
+    STRAVA_ACCESS_TOKEN = "strava_access_token"
