f5260721fe9f272c47aaf985ed8a77b0ee2f6b07
Santosh Gummidipundi <santosh@santoshg.io>
2025-01-18 15:24:58 -0800

    tentative task handlers file

--- task_handler.py
+++ task_handler.py
@@ -1,18 +1,16 @@
 """Application entry point."""
 from os import environ
 
-from utils.cloud_utils import get_secret
+from strava.request_utils import get_all_activities, refresh_access_token_at_expiration
 from utils.logging_utils import logger
 
 
-def handler():
+def handler() -> None:
     """Entry point for the application."""
     logger.info("Task handler started")
-
-    project_id = environ["PROJECT_ID"]
-    logger.info(f"Project ID: {project_id}")
-
-    get_secret(project_id, "strava_api")
+    logger.info(f"Project ID: {environ['PROJECT_ID']}")
+    refresh_access_token_at_expiration()
+    get_all_activities()
 
 
 if __name__ == "__main__":
