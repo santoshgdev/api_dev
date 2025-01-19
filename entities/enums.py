ac027faaae5d8cbf3229a08c6f5b8c6dc1613d77
Santosh Gummidipundi <santosh@santoshg.io>
2025-01-18 22:35:31 -0800

    add dynamic enum

---
+++ entities/enums.py
@@ -0,0 +1,13 @@
+"""Module for Strava enums."""
+from enum import Enum
+
+
+class DynamicEnum(Enum):
+    """A blank enum that adds members via initialization function."""
+
+    pass
+
+    @classmethod
+    def from_dict(cls, data):
+        """Dynamically add members to the Enum from a dictionary."""
+        return Enum(value=cls.__name__, names=list(data.items()))
